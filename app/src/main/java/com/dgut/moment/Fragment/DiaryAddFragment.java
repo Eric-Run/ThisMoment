2544293b0a663b5124450e70ece763a20f6052db
Eric-Run <136560447@qq.com>
2020-03-30 22:21:16 +0800

    日记界面_查看日记的添加界面

--- app/src/main/java/com/dgut/moment/Fragment/DiaryAddFragment.java
+++ app/src/main/java/com/dgut/moment/Fragment/DiaryAddFragment.java
@@ -2,18 +2,26 @@ package com.dgut.moment.Fragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.dgut.moment.R;
+import com.dgut.moment.Util.DatePickerUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import androidx.fragment.app.Fragment;
 
 @SuppressLint("ValidFragment")
 public class DiaryAddFragment extends Fragment {
     private String mTitle;
+    private TextView addMood;
+    private TextView addWeather;
+    private TextView addDate;
 
     public static DiaryAddFragment getInstance(String title) {
         DiaryAddFragment sf = new DiaryAddFragment();
@@ -29,9 +37,25 @@ public class DiaryAddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fr_diary_add, null);
-        TextView card_title_tv = (TextView) v.findViewById(R.id.card_title_tv);
-        card_title_tv.setText(mTitle);
 
+        addMood = v.findViewById(R.id.diary_add_mood);
+        addWeather = v.findViewById(R.id.diary_add_weather);
+        addDate = v.findViewById(R.id.diary_add_date);
+
+        initAddDate();
         return v;
+    }
+
+    //设置初始日期，监听选择日期
+    private void initAddDate(){
+        SimpleDateFormat formatter = new SimpleDateFormat ("yyyy-MM-dd");
+        Date curDate =  new Date(System.currentTimeMillis());
+        addDate.setText(formatter.format(curDate));
+        addDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerUtil().showDatePickDlg(addDate,getContext());
+            }
+        });
     }
 }
--- app/src/main/java/com/dgut/moment/MainActivity.java
+++ app/src/main/java/com/dgut/moment/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.dgut.moment.Util.ViewCenterUtils;
