8cda5c6397c793e9a9e3faffef9512aa4f201d4f
Eric-Run <136560447@qq.com>
2020-04-25 07:36:43 +0800

    不知道修改了什么

--- app/src/main/java/com/dgut/moment/Fragment/BillAddFragment.java
+++ app/src/main/java/com/dgut/moment/Fragment/BillAddFragment.java
@@ -107,53 +107,59 @@ public class BillAddFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                final BillDetail billDetail = new BillDetail();
-                billDetail.setTag(billAddRemark.getText().toString());
-
-                int id = billAddType.getCheckedRadioButtonId();
-                if(id == R.id.bill_add_in){
-                    billDetail.setSum(ConvertUtil.convertToFloat(billAddSum.getText().toString(),-1));
-                }else if(id == R.id.bill_add_out){
-                    String number = "-"+billAddSum.getText().toString();
-                    billDetail.setSum(ConvertUtil.convertToFloat(number,-1));
-                }
-                Log.d("Bill_Add",billDetail.getSum()+"");
-
-                billDetail.setBday(billAddDate.getText().toString());
-                billDetail.save();
-                Log.d("Bill_Add","保存一条账单详情"+billDetail.toString());
-                User user = LitePal.find(User.class,1);
-                user.setBillcount(user.getBillcount()+1);
-                user.save();
-                Log.d("Bill_Add","用户："+user.getUsername()+"增加了一条账单");
-
-                float sum = ConvertUtil.convertToFloat(billAddSum.getText().toString(),-1);
-
-                List<Bill> bills = LitePal.where("billday = ?",billDetail.getBday()).find(Bill.class);
-                if(!bills.isEmpty()){
-                    if(billDetail.getSum()>0) {
-                        bills.get(0).setIncome(bills.get(0).getIncome() + sum);
-                    }else {
-                        bills.get(0).setOutgo(bills.get(0).getOutgo() + sum);
-                    }
-                    bills.get(0).save();
-                    Log.d("Bill_Add","保存一条账单"+bills.get(0).toString());
-
+                if("".equals(billAddRemark.getText().toString())){
+                    ToastUtil.ToastCenter(getContext(),"还没填写备注！");
+                }else if("".equals(billAddSum.getText().toString())){
+                    ToastUtil.ToastCenter(getContext(),"还没填写金额！");
                 }else {
-                    Bill bill = new Bill();
-                    bill.setBid(ConvertUtil.convertToInt(billDetail.getBday().replace("-",""),-1));
-                    bill.setBillday(billDetail.getBday());
-                    if(billDetail.getSum()>0) {
-                        bill.setIncome(sum);
-                    }else {
-                        bill.setOutgo(sum);
+                    final BillDetail billDetail = new BillDetail();
+                    billDetail.setTag(billAddRemark.getText().toString());
+
+                    int id = billAddType.getCheckedRadioButtonId();
+                    if (id == R.id.bill_add_in) {
+                        billDetail.setSum(ConvertUtil.convertToFloat(billAddSum.getText().toString(), -1));
+                    } else if (id == R.id.bill_add_out) {
+                        String number = "-" + billAddSum.getText().toString();
+                        billDetail.setSum(ConvertUtil.convertToFloat(number, -1));
                     }
-                    bill.save();
-                    Log.d("Bill_Add","保存一条账单"+bill.toString());
+                    Log.d("Bill_Add", billDetail.getSum() + "");
+
+                    billDetail.setBday(billAddDate.getText().toString());
+                    billDetail.save();
+                    Log.d("Bill_Add", "保存一条账单详情" + billDetail.toString());
+                    User user = LitePal.find(User.class, 1);
+                    user.setBillcount(user.getBillcount() + 1);
+                    user.save();
+                    Log.d("Bill_Add", "用户：" + user.getUsername() + "增加了一条账单");
+
+                    float sum = ConvertUtil.convertToFloat(billAddSum.getText().toString(), -1);
+
+                    List<Bill> bills = LitePal.where("billday = ?", billDetail.getBday()).find(Bill.class);
+                    if (!bills.isEmpty()) {
+                        if (billDetail.getSum() > 0) {
+                            bills.get(0).setIncome(bills.get(0).getIncome() + sum);
+                        } else {
+                            bills.get(0).setOutgo(bills.get(0).getOutgo() + sum);
+                        }
+                        bills.get(0).save();
+                        Log.d("Bill_Add", "保存一条账单" + bills.get(0).toString());
+
+                    } else {
+                        Bill bill = new Bill();
+                        bill.setBid(ConvertUtil.convertToInt(billDetail.getBday().replace("-", ""), -1));
+                        bill.setBillday(billDetail.getBday());
+                        if (billDetail.getSum() > 0) {
+                            bill.setIncome(sum);
+                        } else {
+                            bill.setOutgo(sum);
+                        }
+                        bill.save();
+                        Log.d("Bill_Add", "保存一条账单" + bill.toString());
+                    }
+                    ToastUtil.ToastCenter(getContext(), "成功记下一笔账");
+                    billAddRemark.setText("");
+                    billAddSum.setText("");
                 }
-                ToastUtil.ToastCenter(getContext(),"成功记下一笔账");
-                billAddRemark.setText("");
-                billAddSum.setText("");
             }
         });
     }
